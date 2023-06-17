be93561c245f530ac4132ee6d1efff8a100f4ac7
TIAGO <tiago.sbittencourt@icloud.com>
2023-06-17 02:00:30 -0300

    add eventListener operations buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,16 @@ document.addEventListener('click', (e) => {
     const target = e.target
 
     if (target.classList.contains('button-num')) {
-        const num = target.innerText
-        
+        const num = target.innerText       
         addDisplay(num)
+    }
+
+    if (target.classList.contains('button-operation')) {
+        if (display.value !== '') {
+            addDisplay(target.innerText)
+        } else {
+            alert('Digite um número primeiro!')
+        }
     }
 
     if (target.classList.contains('button-cls')) {
